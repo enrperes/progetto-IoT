01d2ee8e46485b9dcb9600c73d1467fa409eac92
enrperes <enrico.peressin@gmail.com>
2025-06-20 01:09:42 +0200

    Arduino fix

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -8,36 +8,31 @@ int TEMP = 8;
 DHT dht(TEMP, DHTTYPE);
 
 void setup() {
-  Serial.begin(9600);
-  dht.begin();
-  pinMode(MOTORE, OUTPUT);
-  pinMode(UM_SUOLO, INPUT);
-  pinMode(TEMP, INPUT);
+    Serial.begin(9600);
+    dht.begin();
+    pinMode(MOTORE, OUTPUT);
+    pinMode(UM_SUOLO, INPUT);
+    pinMode(TEMP, INPUT);
 
-  digitalWrite(MOTORE, HIGH);
-  delay(500);
+    digitalWrite(MOTORE, HIGH);
+    delay(500);
 }
 
-void loop(){
+void loop() {
     int t = dht.readTemperature();
     int h = dht.readHumidity();
     float umidita_suolo = analogRead(UM_SUOLO);
-    Serial.print("TEMPERATURA:"); 
+    Serial.print("TEMPERATURA:");
     Serial.print(t);
-    Serial.print(" HUMIDITY:"); 
-    Serial.print  (h);
-    Serial.print(" MOISTURELEVEL:"); 
+    Serial.print(" HUMIDITY:");
+    Serial.print(h);
+    Serial.print(" MOISTURELEVEL:");
     Serial.println(umidita_suolo);
-    if(umidita_suolo>750)
-    {
-    digitalWrite(MOTORE, LOW);
-    }
-    else
-    {
-    digitalWrite(MOTORE, HIGH);
+    if (umidita_suolo > 750) {
+        digitalWrite(MOTORE, LOW);
+    } else {
+        digitalWrite(MOTORE, HIGH);
     }
     Serial.println();
     delay(1000);
-
-    
 }
